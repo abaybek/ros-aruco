275e8b1bf8b532ed36606304c77a2e3edee0a82a
Bekbaganbetov Abay <abay.bekbaganbetov@gmail.com>
2022-10-03 11:51:45 +0300

    iteration

--- scripts/create_map.py
+++ scripts/create_map.py
@@ -2,9 +2,12 @@
 
 import rospy
 import cv2
+import tf
 import tf2_ros
 import numpy as np
 from sensor_msgs.msg import Image, CameraInfo
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PoseStamped
 from cv_bridge import CvBridge
 from functools import partial
 
@@ -21,7 +24,7 @@ def get_camera_configs():
     return camera_matrix, camera_distortion
 
 
-def process_frame(image, camera_matrix: np.array, camera_distortion: np.array):
+def process_frame(image, camera_matrix: np.array, camera_distortion: np.array, publisher):
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -34,42 +37,91 @@ def process_frame(image, camera_matrix: np.array, camera_distortion: np.array):
     )
     if ids is None:
         rospy.loginfo("nothing found")
-        return
+        return image
     
-    rvec, tvec, cc = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_size, camera_matrix, camera_distortion)
-    # rospy.loginfo(rvec)
-    rospy.loginfo(tvec)
-    # rospy.loginfo(cc)
-    return 
-
-
-    ret = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_size, camera_matrix, camera_distortion)
+    rvecs, tvecs, cc = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_size, camera_matrix, camera_distortion)
     cv2.aruco.drawDetectedMarkers(image, corners, ids)
-    for rvec, tvec in zip(ret[0], ret[1]):
-        cv2.aruco.drawAxis(image, camera_matrix, camera_distortion, rvec, tvec, 10)
+    for rvec, tvec, _id in zip(rvecs, tvecs, ids):
+        cv2.aruco.drawAxis(image, camera_matrix, camera_distortion, rvec, tvec, 0.3)
+        pub_func(rvec[0], tvec[0], _id, publisher)
 
+    print(ids)
     return image
 
 
-def callback(data, camera_matrix: np.array, camera_distortion: np.array):
+def callback(data, camera_matrix: np.array, camera_distortion: np.array, publisher, image_pub):
     br = CvBridge()
     rospy.loginfo("receiving video frame")
 
     current_frame = br.imgmsg_to_cv2(data)
     current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
-    process_frame(current_frame, camera_matrix, camera_distortion)
+    current_frame = process_frame(current_frame, camera_matrix, camera_distortion, publisher)
 
+    image_pub.publish(br.cv2_to_imgmsg(current_frame))
+    # for _id, rvec, tvec in process_frame(current_frame, camera_matrix, camera_distortion):
+    #     pub_func(rvec, tvec, _id, publisher)
+
+
+def pub_func(rvec, tvec, _id, publisher):
+    # we need a homogeneous matrix but OpenCV only gives us a 3x3 rotation matrix
+    # rotation_matrix = np.array([[0, 0, 0, 0],
+    #                             [0, 0, 0, 0],
+    #                             [0, 0, 0, 0],
+    #                             [0, 0, 0, 1]],
+    #                             dtype=float)
+    # rotation_matrix[:3, :3], _ = cv2.Rodrigues([rvec[2], rvec[1], rvec[0]])
+
+    # convert the matrix to a quaternion
+    # quaternion = tf.transformations.quaternion_from_matrix(rotation_matrix)
+    # quaternion = tf.transformations.quaternion_from_euler(rvec[2], rvec[1], rvec[0])
+    print(rvec)
+    print(tvec)
+    print(_id)
+    quaternion = tf.transformations.quaternion_from_euler(rvec[2], rvec[1] + 1.5, rvec[0])
+
+    # To visualize in rviz, you can, for example, publish a PoseStamped message:
+    pose = Marker()
+    # breakpoint()
+    pose.ns = "basic_shapes"
+    pose.id = int(_id)
+    pose.type = Marker.CUBE
+    pose.action = Marker.ADD
+    # pose.lifetime.secs = 100.0
+    pose.header.frame_id = "camera_link"
+    pose.pose.position.x = tvec[2]
+    pose.pose.position.y = tvec[1]
+    pose.pose.position.z = -tvec[0]
+    pose.pose.orientation.x = quaternion[0]
+    pose.pose.orientation.y = quaternion[1]
+    pose.pose.orientation.z = quaternion[2]
+    pose.pose.orientation.w = quaternion[3]
+    pose.scale.x = 0.2
+    pose.scale.y = 0.2
+    pose.scale.z = 0.02
+
+    pose.color.r = 1.0
+    pose.color.g = 1.0
+    pose.color.b = 1.0
+    pose.color.a = 1.0
+    print(pose)
+    publisher.publish(pose)
 
 
 def main():
     rospy.init_node("create_map_node", anonymous=True)
     camera_matrix, camera_distortion = get_camera_configs()
 
-    rospy.Subscriber("image_raw", Image, partial(callback, camera_matrix=camera_matrix, camera_distortion=camera_distortion))
+    marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size = 10)
+    image_pub = rospy.Publisher("/image_processed", Image, queue_size = 1)
+    rospy.Subscriber("image_raw", Image, partial(callback, camera_matrix=camera_matrix, camera_distortion=camera_distortion, publisher=marker_pub, image_pub=image_pub))
     rospy.loginfo("*"*100)
     
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    rospy.spin() # simply keeps python from exiting until this node is stopped
+    # while not rospy.is_shutdown():
+        # do whatever you want here
+        # pub.publish(foo)
+        # rospy.sleep(0.01)  # sleep for one second
+
 
     # Close down the video stream when done
     cv2.destroyAllWindows()
